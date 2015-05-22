587a012db54c30cecd64cab33a92773935c9a8b9
unknown <jason.sackey@RROOM006.readingroom.local>
2015-05-22 13:56:01 +0100

    integrate disclaimer message and menu

---
+++ _source/javascripts/alpha-disclaimer.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    var disclaimeraccepted = $.cookie("acceptedDisclaimer");
+
+    if (!disclaimeraccepted) $('#disclaimerModal').modal('show');
+    $('#aboutPrototype').click(function () {
+        $('#disclaimerModal').modal('show');
+    });
+    $('#disclaimerModal').on('hidden.bs.modal', function () {
+        $.cookie("acceptedDisclaimer", 1);
+    });
+
+});
