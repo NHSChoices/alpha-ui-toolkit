77360c3225851496caf43717dc471fe6feb8d8ce
unknown <jason.sackey@RROOM006.readingroom.local>
2015-05-22 10:28:39 +0100

    Toolkit component partial used for Headings

--- www/javascripts/ui-toolkit.js
+++ www/javascripts/ui-toolkit.js
@@ -4,8 +4,7 @@ $(function () {
         var url = '/stylesheets/site-white.css';
         if (document.createStyleSheet) {
             document.createStyleSheet(url);
-        }
-        else {
+        } else {
             $('<link rel="stylesheet" type="text/css" href="' +
             url + '" />').appendTo('head'); 
         }
