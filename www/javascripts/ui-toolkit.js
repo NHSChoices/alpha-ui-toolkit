ebbdd9e958df4b4441a2980de109bfc12a60861a
jamestiffen <james.tiffen@hscic.gov.uk>
2015-05-22 17:00:09 +0100

    Updated styles switch to persist for session

--- www/javascripts/ui-toolkit.js
+++ www/javascripts/ui-toolkit.js
@@ -1,12 +1,22 @@
 $(function () {
-    // load white stylesheet when url has '?white'
-    if (document.location.search.indexOf('white') > 0) {
-        var url = '/stylesheets/site-white.css';
+
+    //Determine if styles are requested and set cookie
+    if (document.location.search.length > 0) {
+        $.cookie("styleToLoad", document.location.search.replace("?", ""));
+    }
+
+    //load styles from cookie
+    var stylesToLoad = "";
+    if ($.cookie("styleToLoad")) stylesToLoad = $.cookie("styleToLoad").split('&');
+
+    //apply styles requested / persisted
+    for (var i = 0; i < stylesToLoad.length; i++) {
+        var url = '/stylesheets/site-' + stylesToLoad[i] + '.css';
         if (document.createStyleSheet) {
             document.createStyleSheet(url);
         } else {
             $('<link rel="stylesheet" type="text/css" href="' +
-            url + '" />').appendTo('head'); 
+                url + '" />').appendTo('head');
         }
     }
 });
