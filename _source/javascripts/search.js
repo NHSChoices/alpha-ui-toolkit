ed3592bc4bcb642c84c486051a120d399f5e3aa9
Nigel Atwell <nigel.atwell@nhs.net>
2015-06-22 15:40:44 +0100

    :mag: refactor search script into toolkit

---
+++ _source/javascripts/search.js
@@ -0,0 +1,41 @@
+﻿function Geolocation() {
+
+    var self = this;
+
+    this.latitude = null;
+    this.longitude = null;
+
+    this.readCurrentPosition = function () {
+        var setGeoLocation = function (position) {
+            self.latitude = position.coords.latitude;
+            self.longitude = position.coords.longitude;
+
+            $('#latitude').val(position.coords.latitude);
+            $('#longitude').val(position.coords.longitude);
+            $('#allowGeoLocation').val('true');
+        };
+
+        var getGeoLocationError = function (err) {
+            $('#allowGeoLocation').val('false');
+            console.error(err.message);
+        }
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(setGeoLocation, getGeoLocationError);
+        } else {
+            getGeoLocationError({ message: 'GeoLocation not supported' });
+        }
+    }
+};
+
+
+$(function () {
+    $("#q_nav").autocomplete({ source: "http://alpha.nhs.uk/search/home/suggest" });
+    $("#q_page").autocomplete({ source: "http://alpha.nhs.uk/search/home/suggest" });
+  
+    console.log("Goelocation currently disabled");
+    return;
+
+    var geolocation = new Geolocation();
+    geolocation.readCurrentPosition();
+});
